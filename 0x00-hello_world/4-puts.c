b6a4c9f8a88f4a82927b7a414ed7483f69aeec56
Zakariyagaidi <Zakariyagaidi>
2023-06-16 06:57:52 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,21 @@
 #include <stdio.h>
+
+
 /**
- * main - Entry point
- *
- * Description: A C program that prints with put function
- *
- *Return: Alawys 0 (Success)
-*/
-int main(void)
-{   
-       	puts("\"Programming is like building a multilingual puzzle"); 
+ * main - prints "Programming is like building a multilingual
+ * puzzle, followed by a newline.
+ * Return: Always 0
+ */
+
+
+int main(void) 
+
+
+{
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
+
+
 }
+
+
